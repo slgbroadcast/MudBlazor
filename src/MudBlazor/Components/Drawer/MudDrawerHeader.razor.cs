34fa177ed7d6aed2005ef9fae13a8824ca9fd662
TheBigNeo <alexandro.will@outlook.com>
2024-11-26 13:00:11 +0100

    Set default values for Dense and Ripple parameters

--- src/MudBlazor/Components/Drawer/MudDrawerHeader.razor.cs
+++ src/MudBlazor/Components/Drawer/MudDrawerHeader.razor.cs
@@ -30,7 +30,7 @@ public partial class MudDrawerHeader
     /// </remarks>
     [Parameter]
     [Category(CategoryTypes.Drawer.Appearance)]
-    public bool Dense { get; set; }
+    public bool Dense { get; set; } = true;
 
     /// <summary>
     /// Navigates to the index page on click.
